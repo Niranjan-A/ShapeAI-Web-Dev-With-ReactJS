1f975df98e05885eb531cc541c63f7185b01d936
Niranjan-A <84324840+Niranjan-A@users.noreply.github.com>
2021-06-07 15:55:51 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyrights @2021</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className = "note">
+            <h1>Javascript and React.js</h1>
+            <p>
+                This was full-on 7-day comprehensive bootcamp
+                on web development with React.js organized by ShapeAI 
+            </p>
+        </div>
+    );
+}
+
+export default Note;
